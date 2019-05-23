ff3ae072cf8a510b1c47f973d2b18aa06a587f88
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-05-23 10:58:14 +0300

    wip

--- netcore3_clipboardsetdata/Class1.cs
+++ netcore3_clipboardsetdata/Class1.cs
@@ -1,6 +1,17 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Windows;
 
 namespace netcore3_clipboardsetdata {
-    public class Class1 {
+    [TestFixture]
+    public class TestClass {
+        [Test]
+        public void Test() {
+            DataObject data = new DataObject();
+            data.SetData(typeof(string), "SampleText");
+            Clipboard.SetDataObject(data);
+            string text = ((DataObject)Clipboard.GetDataObject()).GetText();
+            Assert.AreEqual("SampleText", text);
+        }
     }
 }
